3de6021d9de6a3a82eaec101f0b0b29d5f1490d2
Namanrockzzz <naman.taneja123@gmail.com>
2021-01-16 20:14:52 +0530

    Product Screen Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 // import SigninScreen from './screens/SigninScreen';
 function App() {
   return (
@@ -21,6 +24,90 @@ function App() {
             {data.products.map((product) => (
               <div key={product._id} className="card">
                 <a href={`/product/${product._id}`}>
+                <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <Link to="/" ><img className="FoodWeb" src="../images/FoodWeb2.png" height="70" alt="FoodWeb"/></Link>
+          </div>
+          <div className="brand">
+            <button className="lines" onClick={openMenu}>
+              &#9776;
+            </button>
+          </div>
+          <div className="header-links">
+            <a href="cart/cart.html">Cart</a>
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
+            {userInfo && userInfo.isAdmin && (
+              <div className="dropdown">
+                <a href="#"  >Admin</a>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orders">Orders</Link>
+                    <Link to="/products">Products</Link>
+                  </li>
+                </ul>
+                
+              </div>
+            )}
+          </div>
+        </header>
+        <aside className="sidebar">
+          <center><h3 className="SC"><br/>Shopping Categories<br/><br/></h3></center>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <br/><br/>
+          <div className="DivC"><Link to="/category/pizza" className="textc">Pizza</Link></div>
+          <div className="DivC"><Link to="/category/indian" className="textc">Indian</Link></div>
+          <div className="DivC"><Link to="/category/burger" className="textc">Burger</Link></div>
+          <div className="DivC"><Link to="/category/dessert" className="textc">Desserts</Link></div>
+          {/* <ul className="categories">
+            <li>
+            <Link to="/category/pizza">Pizza</Link>
+            </li>
+
+            <li>
+              <Link to="/category/indian">Indian</Link>
+            </li>
+
+            <li>
+              <Link to="/category/burger">Burger</Link>
+            </li>
+
+            <li>
+              <Link to="/category/dessert">Desserts</Link>
+            </li>
+
+          </ul> */}
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/orders" component={OrdersScreen} />
+            <Route path="/profile" component={ProfileScreen} />
+            <Route path="/order/:id" component={OrderScreen} />
+            <Route path="/products" component={ProductsScreen} />
+            <Route path="/shipping" component={ShippingScreen} />
+            <Route path="/payment" component={PaymentScreen} />
+            <Route path="/placeorder" component={PlaceOrderScreen} />
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/category/:id" component={HomeScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+
+
+          </div>
+
+        </main>
+        <footer className="footer">
+          All right reserved.
+          © Copyright 2021 Design by Team ThunderBolt
+    </footer>
+      </div>
+    </BrowserRouter>
                   <img
                     className="medium"
                     src={product.image}
